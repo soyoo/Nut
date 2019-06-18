b3193cf1c6a491f231de033ff71d83569efac721
Hamed Masafi <hamed.masafi@gmail.com>
2019-06-18 20:41:14 +0430

    some shared pointer [skip ci]

--- src/defines.h
+++ src/defines.h
@@ -45,6 +45,83 @@
     Q_CLASSINFO(__nut_NAME_PERFIX type #name #value,                           \
                 type "\n" #name "\n" value)
 
+#define NUT_FIELD_PERFIX
+#define NUT_FIELD_POSTFIX Field
+
+// Database
+#define NUT_DB_VERSION(version)  \
+    NUT_INFO(__nut_DB_VERSION, version, 0)
+
+#define NUT_DECLARE_TABLE(type, name)                                          \
+    NUT_INFO(__nut_TABLE, type, name)                                          \
+    Q_PROPERTY(NUT_WRAP_NAMESPACE(TableSet<type>) name READ name)              \
+    NUT_WRAP_NAMESPACE(TableSet<type>) *m_##name;                              \
+    public:                                                                    \
+    static const type *_##name;                                                \
+    NUT_WRAP_NAMESPACE(TableSet<type>) *name() const                           \
+    { return m_##name; }                                                       \
+    private:
+
+//Table
+#define NUT_DECLARE_FIELD(type, name, read, write)                             \
+    Q_PROPERTY(type name READ read WRITE write)                                \
+    NUT_INFO(__nut_FIELD, name, 0)                                             \
+    type m_##name;                                                             \
+public:                                                                        \
+    static NUT_WRAP_NAMESPACE(FieldPhrase<type>)& name ## Field(){             \
+        static NUT_WRAP_NAMESPACE(FieldPhrase<type>) f =                       \
+                NUT_WRAP_NAMESPACE(FieldPhrase<type>)                          \
+                        (staticMetaObject.className(), #name);                 \
+        return f;                                                              \
+    }                                                                          \
+    type read() const{                                                         \
+        return m_##name;                                                       \
+    }                                                                          \
+    void write(type name){                                                     \
+        m_##name = name;                                                       \
+        propertyChanged(#name);                                                \
+    }
+
+#define NUT_FOREGION_KEY(type, keytype, name, read, write)                     \
+    Q_PROPERTY(Nut::Row<type> name READ read WRITE write)                      \
+    NUT_DECLARE_FIELD(keytype, name##Id, read##Id, write##Id)                  \
+    NUT_INFO(__nut_FOREGION_KEY, name, type)                                   \
+    Nut::Row<type> m_##name;                                                   \
+public:                                                                        \
+    Nut::Row<type> read() const { return m_##name ; }                          \
+    void write(Nut::Row<type> name){                                           \
+        m_##name = name;                                                       \
+    }
+
+#define NUT_DECLARE_CHILD_TABLE(type, n)                                       \
+    private:                                                                   \
+        NUT_WRAP_NAMESPACE(TableSet)<type> *m_##n;                             \
+    public:                                                                    \
+        static type *n##Table();                                               \
+        NUT_WRAP_NAMESPACE(TableSet)<type> *n();
+
+#define NUT_IMPLEMENT_CHILD_TABLE(class, type, n)                              \
+    type *class::n##Table(){                                                   \
+        static auto f = new type();                                           \
+        return f;                                                              \
+    }                                                                          \
+    NUT_WRAP_NAMESPACE(TableSet)<type> *class::n(){                            \
+        return m_##n;                                                          \
+    }
+
+#define NUT_FIELD(name)                     NUT_INFO(__nut_FIELD, name, 0)
+#define NUT_PRIMARY_KEY(x)                  NUT_INFO(__nut_PRIMARY_KEY,  x, 0)
+#define NUT_AUTO_INCREMENT(x)               NUT_INFO(__nut_AUTO_INCREMENT, x, 0)
+#define NUT_PRIMARY_AUTO_INCREMENT(x)       NUT_INFO(__nut_PRIMARY_KEY_AI, x, 0)
+#define NUT_DISPLAY_NAME(field, name)       NUT_INFO(__nut_DISPLAY, field, name)
+#define NUT_UNIQUE(x)                       NUT_INFO(__nut_UNIQUE, x, 0)
+#define NUT_LEN(field, len)                 NUT_INFO(__nut_LEN, field, len)
+#define NUT_DEFAULT_VALUE(x, n)             NUT_INFO(__nut_DEFAULT_VALUE, x, n)
+#define NUT_NOT_NULL(x)                     NUT_INFO(__nut_NOT_NULL, x, 1)
+#define NUT_INDEX(name, field, order)
+
+NUT_BEGIN_NAMESPACE
+
 inline bool nutClassInfo(const QMetaClassInfo &classInfo,
                          QString &type, QString &name, QVariant &value)
 {
@@ -117,79 +194,37 @@ inline bool nutClassInfoInt(const QMetaClassInfo &classInfo,
     }
 }
 
-#define NUT_FIELD_PERFIX
-#define NUT_FIELD_POSTFIX Field
+#ifdef NUT_SHARED_POINTER
+template <class T>
+using RowList = QList<QSharedPointer<T>>;
 
-// Database
-#define NUT_DB_VERSION(version)  \
-    NUT_INFO(__nut_DB_VERSION, version, 0)
+template <class T>
+using RowSet = QSet<QSharedPointer<T>>;
 
-#define NUT_DECLARE_TABLE(type, name)                                          \
-    NUT_INFO(__nut_TABLE, type, name)                                          \
-    Q_PROPERTY(NUT_WRAP_NAMESPACE(TableSet<type>) name READ name)              \
-    NUT_WRAP_NAMESPACE(TableSet<type>) *m_##name;                              \
-    public:                                                                    \
-    static const type *_##name;                                                \
-    NUT_WRAP_NAMESPACE(TableSet<type>) *name() const                           \
-    { return m_##name; }                                                       \
-    private:
+template <typename T>
+using Row = QSharedPointer<T>;
 
-//Table
-#define NUT_DECLARE_FIELD(type, name, read, write)                             \
-    Q_PROPERTY(type name READ read WRITE write)                                \
-    NUT_INFO(__nut_FIELD, name, 0)                                             \
-    type m_##name;                                                             \
-public:                                                                        \
-    static NUT_WRAP_NAMESPACE(FieldPhrase<type>)& name ## Field(){             \
-        static NUT_WRAP_NAMESPACE(FieldPhrase<type>) f =                       \
-                NUT_WRAP_NAMESPACE(FieldPhrase<type>)                          \
-                        (staticMetaObject.className(), #name);                 \
-        return f;                                                              \
-    }                                                                          \
-    type read() const{                                                         \
-        return m_##name;                                                       \
-    }                                                                          \
-    void write(type name){                                                     \
-        m_##name = name;                                                       \
-        propertyChanged(#name);                                                \
-    }
+template<class T>
+inline Row<T> create() {
+    return QSharedPointer<T>(new T);
+}
 
-#define NUT_FOREGION_KEY(type, keytype, name, read, write)                     \
-    Q_PROPERTY(type* name READ read WRITE write)                               \
-    NUT_DECLARE_FIELD(keytype, name##Id, read##Id, write##Id)                  \
-    NUT_INFO(__nut_FOREGION_KEY, name, type)                                   \
-    type *m_##name;                                                            \
-public:                                                                        \
-    type *read() const { return m_##name ; }                                   \
-    void write(type *name){                                                    \
-        m_##name = name;                                                       \
-    }
+#else
+template <typename T>
+using RowList = QList<T*>;
 
-#define NUT_DECLARE_CHILD_TABLE(type, n)                                       \
-    private:                                                                   \
-        NUT_WRAP_NAMESPACE(TableSet)<type> *m_##n;                             \
-    public:                                                                    \
-        static type *n##Table();                                               \
-        NUT_WRAP_NAMESPACE(TableSet)<type> *n();
+template <typename T>
+using RowSet = QSet<T*>;
 
-#define NUT_IMPLEMENT_CHILD_TABLE(class, type, n)                              \
-    type *class::n##Table(){                                                   \
-        static auto f = new type();                                           \
-        return f;                                                              \
-    }                                                                          \
-    NUT_WRAP_NAMESPACE(TableSet)<type> *class::n(){                            \
-        return m_##n;                                                          \
-    }
+template <typename T>
+using Row = T*;
 
-#define NUT_FIELD(name)                     NUT_INFO(__nut_FIELD, name, 0)
-#define NUT_PRIMARY_KEY(x)                  NUT_INFO(__nut_PRIMARY_KEY,  x, 0)
-#define NUT_AUTO_INCREMENT(x)               NUT_INFO(__nut_AUTO_INCREMENT, x, 0)
-#define NUT_PRIMARY_AUTO_INCREMENT(x)       NUT_INFO(__nut_PRIMARY_KEY_AI, x, 0)
-#define NUT_DISPLAY_NAME(field, name)       NUT_INFO(__nut_DISPLAY, field, name)
-#define NUT_UNIQUE(x)                       NUT_INFO(__nut_UNIQUE, x, 0)
-#define NUT_LEN(field, len)                 NUT_INFO(__nut_LEN, field, len)
-#define NUT_DEFAULT_VALUE(x, n)             NUT_INFO(__nut_DEFAULT_VALUE, x, n)
-#define NUT_NOT_NULL(x)                     NUT_INFO(__nut_NOT_NULL, x, 1)
-#define NUT_INDEX(name, field, order)
+template<class T>
+inline Row<T> create() {
+    return new T;
+}
+#endif
+
+NUT_END_NAMESPACE
 
 #endif // SYNTAX_DEFINES_H
